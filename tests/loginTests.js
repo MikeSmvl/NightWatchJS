61643254252263c514a14ff0f8268db3351cb8f7
Mikael Samvelian <mikael.samvelian@gmail.com>
2019-04-09 14:21:38 -0400

    #3 Add image vlidation tests
    This commit adds the tests that use the custom assertion and command called compareScreenshot. It will take a screenshot on the page after running the tests and make sure it matches a baseline that is supplied before running the test called a baseline image.

--- tests/loginTests.js
+++ tests/loginTests.js
@@ -4,20 +4,36 @@ let pwd = '12345';
 
 module.exports = {
   '@disabled': false,
+  // Connect to the webpage and login before each test case
+  beforeEach: function(client, done) {
+    client
+      .resizeWindow(1680, 968, done) // Baseline images were taken using these dimensions (1680x1200)
+      .url('http://testing-ground.scraping.pro/login')
+      .waitForElementPresent('body', 1000);
+  },
 
   'Successful credential login with text validation': function(client) {
-    client
-      .url('http://testing-ground.scraping.pro/login')
-      .waitForElementPresent('body', 1000)
-      .login(usr, pwd)
-      .assert.containsText('#content', 'WELCOME :)');
+    client.login(usr, pwd).assert.containsText('#content', 'WELCOME :)');
+  },
+
+  'Successful credential login with image validation': function(client) {
+    client.login(usr, pwd).pause(1000, function(result) {
+      client.compareScreenshot('login-success-baseline.png');
+    });
   },
 
   'Invalid credential login with text and text validation': function(client) {
     client
-      .url('http://testing-ground.scraping.pro/login')
-      .waitForElementPresent('body', 1000)
-      .login(usr, pwd)
+      .login('invalid', 'credential')
       .assert.containsText('#content', 'ACCESS DENIED!');
+  },
+
+  'Invalid credential login with text and image validation': function(client) {
+    client
+      .login('invalid', 'credential')
+      .pause(1000, function(result) {
+        client.compareScreenshot('login-denied-baseline.png');
+      })
+      .end();
   }
 };
